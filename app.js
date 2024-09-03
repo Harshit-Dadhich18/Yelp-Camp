38c6ba3135c7c785dfdd8c785ac43c7f5554dc16
Harshit-Dadhich18 <hdadheech46@gmail.com>
2024-09-03 13:15:01 +0530

    changes in app  mongoo

--- app.js
+++ app.js
@@ -24,10 +24,12 @@ const campgroundsRoutes = require('./routes/campgrounds');
 const reviewsRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/user');
 // const DbUrl = process.env.DB_URL
-const DbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp1'; 
+const DbUrl = process.env.DB_URL; 
 // mongodb://127.0.0.1:27017/yelp-camp1
 mongoose.connect(DbUrl,
     { useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
         useUnifiedTopology: true}).then(() => {
         console.log("Connected to Mongoose");
     })
